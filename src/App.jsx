69d629441a880a2c21fdda0c34acb88858a7eb27
IncognitoJohn <155202956+IncognitoJohn@users.noreply.github.com>
2026-05-11 00:10:22 +0100

    Edited the pages to be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import {Routes,Route} from "react-router";
 import {HomePage} from "./Pages/HomePage.jsx";
 import './index.css';
 import {CheckoutPage} from "./Pages/CheckoutPage.jsx";
+import {OrdersPage} from "./Pages/OrdersPage.jsx";
+import {TrackingPage} from "./Pages/TrackingPage.jsx";
 function App() {
   return (
       <Routes>
 
       <Route index element={<HomePage/>}/>
-          <Route path="checkout" element={<CheckoutPage/>}/>
+      <Route path="checkout" element={<CheckoutPage/>}/>
+          <Route path="orders" element={<OrdersPage/>}/>
+          <Route path="tracking  " element={<TrackingPage/>}/>
       </Routes>
 
   );
