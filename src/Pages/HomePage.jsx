8d0fbbec4667bb5c4502a09ae2364d1ee7b7f119
IncognitoJohn <155202956+IncognitoJohn@users.noreply.github.com>
2026-05-11 02:36:26 +0100

    Use axios to fetch data from the backend

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,12 @@
 import {Header} from '../components/Header.jsx';
 import '../components/header.css';
 import {products} from "../assets/products.js";
-
+import axios from 'axios';
 import './HomePage.css';
 export function HomePage() {
-     fetch('http://localhost:3000/api/products').then(
-         (response)=>{
-            return   response.json();
-         }).then((data)=>{
-             console.log(data);
-     })
+     axios.get('http://localhost:3000/api/products').then((response)=>{
+       console.log(response.data);
+    })
 
 
     return (<>
